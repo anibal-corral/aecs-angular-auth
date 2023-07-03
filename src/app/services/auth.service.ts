9fb00e27a82e26f6033c7c9105df4bd575f572ba
Anibal Corral Serrano <anibal.corral@gmail.com>
2023-07-03 18:21:01 -0400

    Interceptor added

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -5,6 +5,7 @@ import { BehaviorSubject, switchMap, tap } from 'rxjs';
 import { TokenService } from './token.service';
 import { ResponseLogin } from '../models/auth.model';
 import { User } from '@models/user.model';
+import { checkToken } from '@interceptors/token.interceptor';
 
 @Injectable({
   providedIn: 'root'
@@ -83,11 +84,12 @@ export class AuthService {
   }
 
   getProfile(){
-    const token = this.tokenService.getToken();
+    // const token = this.tokenService.getToken();
     return this.http.get<User>(`${this.apiUrl}/api/v1/auth/profile`,{
-      headers:{
-        Authorization: `Bearer ${token}`
-      }
+      // headers:{
+      //   Authorization: `Bearer ${token}`
+      // }
+      context:checkToken()
     }).pipe(
       tap( user => {
         this.user$.next(user);
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,8 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { TokenService } from './token.service';
+// import { TokenService } from './token.service';
 import { environment } from '@environments/environment';
 import { User } from '@models/user.model';
+import { checkToken } from '@interceptors/token.interceptor';
 
 @Injectable({
   providedIn: 'root'
@@ -12,16 +13,17 @@ apiUrl = environment.API_URL;
   
 constructor(
     private http: HttpClient,
-    private tokenService:TokenService
+    // private tokenService:TokenService
   ) { }
 
 
   getUsers(){
-    const token = this.tokenService.getToken();
+    // const token = this.tokenService.getToken();
 return this.http.get<User[]>(`${this.apiUrl}/api/v1/users`,{
-  headers:{
-    Authorization:`Bearer ${token}`
-  }
+  // headers:{
+  //   Authorization:`Bearer ${token}`
+  // }
+  context:checkToken()
 });
   }
 
