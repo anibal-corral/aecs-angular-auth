b78c49789fb32fb787045ac0840780b7adcf03b4
Anibal Corral Serrano <anibal.corral@gmail.com>
2023-07-18 17:39:07 -0400

    Refresh token implemented

--- src/app/services/token.service.ts
+++ src/app/services/token.service.ts
@@ -42,4 +42,38 @@ export class TokenService {
 
     return false;
   }
+
+  saveRefreshToken(token:string){
+    // localStorage.setItem('token', token);
+    setCookie('refresh-token', token, {expires:365, path:'/'});
+
+  }
+
+  getRefreshToken(){
+    // const token = localStorage.getItem('token');
+    const token = getCookie('refresh-token');
+    return token;
+  }
+
+  removeRefreshToken(){
+    // localStorage.removeItem('token');
+    removeCookie('refresh-token');  
+  }
+
+  isValidRefreshToken(){
+    const token = this.getRefreshToken();
+    if(!token){
+      return false;
+    }
+    //j
+    const decodedToken = jwt_decode<JwtPayload>(token);
+    if(decodedToken && decodedToken.exp){
+      const tokenDate = new Date(0);
+      tokenDate.setUTCSeconds(decodedToken.exp);
+      const today = new Date();
+      return tokenDate.getTime() > today.getTime();
+    }
+
+    return false;
+  }
 }
